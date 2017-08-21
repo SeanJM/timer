b5f4167c183d35c0ec46ed4981f59dbda151a0ae
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-21 08:38:45 -0400

    Linted

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-const express    = require("express");        // call express
-const app        = express();                 // define our app using express
-const bodyParser = require("body-parser");
-const routes = require("./routes");
 const path = require("path");
+const exec = require("child_process").exec;
+
+const bodyParser = require("body-parser");
+const express = require("express");        // call express
+const routes = require("./routes");
+const app = express();                 // define our app using express
+
 const port = 5000;        // set our port
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/dist', express.static(path.resolve('dist/')));
-app.use('/sound', express.static(path.resolve('sound/')));
+app.use("/dist", express.static(path.resolve("dist/")));
+app.use("/sound", express.static(path.resolve("sound/")));
 
 routes(app);
 app.listen(port);
@@ -17,3 +20,5 @@ app.listen(port);
 console.log(
   "Proxy is running on http://localhost:" + port
 );
+
+exec("open http://localhost:" + port);
