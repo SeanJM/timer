fcc7a35ae192db4373dfc5edd746afa0f3ee6c29
SeanJM <seanjmacisaac@gmail.com>
2018-07-18 21:31:08 -0400

    Added generics to "persistore" and "store"

---
+++ frontend/src/class/persistore.ts
@@ -0,0 +1,167 @@
+import { debounce } from "lodash";
+import getPaths from "@scripts/get-paths";
+
+import Store, {
+  OnChangeFunction,
+  OnPathChangeFunction
+} from "@class/store";
+
+const validFirstCharacter = {
+  "\"": true,
+  "[": true,
+  "{": true,
+};
+
+const INVALID = {
+  "[object Object]": true,
+};
+
+const IS_VALID_VALUE = {
+  "true": true,
+  "false": true,
+  "null": true,
+};
+
+function set(
+  target: object,
+  path: string[] | string,
+  value: any
+) {
+  let p = Array.isArray(path)
+    ? path
+    : path.split(".");
+
+  let i = -1;
+  let o = target;
+  const n = p.length - 1;
+
+  while (++i < n) {
+    if (!o[p[i]]) {
+      o[p[i]] = {};
+    } else if (Array.isArray(o[p[i]])) {
+      o[p[i]] = Object.assign({}, o[p[i]]);
+    }
+    o = o[p[i]];
+  }
+
+  o[p[p.length - 1]] = value;
+  return target;
+}
+
+type Ignore = RegExp[];
+
+interface Settings {
+  validator?: (state: object) => boolean;
+  ignore?: Ignore;
+}
+
+export default class PersiStore<T> {
+  /**
+   *
+   * @param {object} initialState
+   * @param {object=} settings
+   * @param {function} settings.validator
+   */
+  store: Store<T>;
+  ignore: Ignore;
+  value: T;
+  saveLocalStorageDebounced: () => void;
+
+  constructor(initialState: T, settings?: Settings) {
+    let persistentState = {};
+    const n = localStorage.length;
+    let i = -1;
+
+    settings = settings || {};
+    this.ignore = settings.ignore || [];
+
+    while (++i < n) {
+      let key = localStorage.key(i);
+      let value = localStorage.getItem(key);
+      if (
+        this.isValidValue(value) &&
+        !INVALID[value.substring(0, 15)] &&
+        !this.isIgnored(key)
+      ) {
+        set(persistentState, key, JSON.parse(value));
+      }
+    }
+
+    if (typeof settings.validator === "function" && !settings.validator(persistentState)) {
+      console.log("Store is invalid - setting initial state");
+      this.store = new Store<T>(initialState);
+      this.value = this.store.value;
+      localStorage.clear();
+      this.saveLocalStorage();
+    } else {
+      console.log("Store is valid - setting initial state");
+      this.store = new Store<T>(persistentState as T);
+      this.value = this.store.value;
+    }
+
+    this.saveLocalStorageDebounced = debounce(() => this.saveLocalStorage(), 500);
+    this.store.onSet((value) => {
+      this.value = value;
+      this.saveLocalStorageDebounced();
+    });
+  }
+
+  isValidValue(value): boolean {
+    return (
+      IS_VALID_VALUE[value] ||
+      /^[0-9.]+$/.test(value) ||
+      validFirstCharacter[value[0]]);
+  }
+
+  isIgnored(setPath): boolean {
+    let i = -1;
+    let n = this.ignore.length;
+
+    while (++i < n) {
+      if (this.ignore[i].test(setPath)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  saveLocalStorage(): this {
+    const paths = getPaths(this.value);
+    let i = -1;
+    let n = paths.length;
+
+    while (++i < n) {
+      let setPath = paths[i].slice(0, -1).join(".");
+      let value = paths[i].slice(-1)[0];
+      if (!this.isIgnored(setPath)) {
+        localStorage.setItem(
+          setPath,
+          JSON.stringify(value)
+        );
+      }
+    }
+
+    return this;
+  }
+
+  set(value: Partial<T>): this {
+    this.store.set(value);
+    return this;
+  }
+
+  onChange(callback: OnChangeFunction<T>): this {
+    this.store.onChange(callback);
+    return this;
+  }
+
+  onPathChange(path: string, callback: OnPathChangeFunction<T>): this {
+    this.store.onPathChange(path, callback);
+    return this;
+  }
+
+  offChange(callback): this {
+    this.store.offChange(callback);
+    return this;
+  }
+}
---
+++ frontend/src/class/store.ts
@@ -0,0 +1,121 @@
+import { set, get } from "lodash";
+import copy from "@scripts/copy";
+import getPaths from "@scripts/get-paths";
+
+export type OnChangeFunction<T> =
+  (prevState: T, nextState: T) => void;
+
+export type OnSetFunction<T> =
+  (nextState: T) => void;
+
+export type OnPathChangeFunction<T> =
+  (prevValue: T, nextValue: T) => void;
+
+export default class Store<T> {
+  value: T;
+  onchangeListeners: OnChangeFunction<T>[];
+  onsetListeners: OnSetFunction<T>[];
+  onpathset: {
+    [key: string]: OnPathChangeFunction<T>[]
+  };
+
+  constructor(state: T) {
+    this.value = state;
+    this.onchangeListeners = [];
+    this.onsetListeners = [];
+    this.onpathset = {};
+  }
+
+  emitPathList(emitPathList: string[], prevState: T, nextState: T) {
+    for (var k in this.onpathset) {
+      if (emitPathList.indexOf(k) !== -1) {
+        let prevValue = get(prevState, k);
+        let nextValue = get(nextState, k);
+        let onpathset = this.onpathset[k].slice();
+        let i = -1;
+        const n = onpathset.length;
+        while (++i < n) {
+          onpathset[i](prevValue, nextValue);
+        }
+      }
+    }
+    return this;
+  }
+
+  emitChange(prevState: T, nextState: T) {
+    const onchangeListeners = this.onchangeListeners.slice();
+    const onsetListeners = this.onsetListeners.slice();
+
+    let i = -1;
+    let n = onsetListeners.length;
+
+    while (++i < n) {
+      onsetListeners[i](nextState);
+    }
+
+    i = -1;
+    n = onchangeListeners.length;
+    while (++i < n) {
+      let callback = onchangeListeners[i];
+      callback(prevState, nextState);
+    }
+  }
+
+  set(value: Partial<T>): this {
+    const prevState = this.value;
+    const paths = getPaths(value);
+    const emitPathList: string[] = [];
+    let i = -1;
+    const n = paths.length;
+
+    this.value = copy(this.value);
+
+    while (++i < n) {
+      let setPath = paths[i].slice(0, paths[i].length - 1);
+      let setValue = paths[i][paths[i].length - 1];
+      let x = 0;
+      let y = setPath.length + 1;
+      while (++x < y) {
+        let setPathString = setPath.slice(0, x).join(".");
+        if (emitPathList.indexOf(setPathString) === -1) {
+          emitPathList.push(setPathString);
+        }
+      }
+      set(this.value, setPath, setValue);
+    }
+
+    this.emitChange(
+      prevState,
+      this.value
+    );
+
+    this.emitPathList(
+      emitPathList,
+      prevState,
+      this.value
+    );
+
+    return this;
+  }
+
+  onSet(callback: OnSetFunction<T>): this {
+    this.onsetListeners.push(callback);
+    return this;
+  }
+
+  onPathChange(path: string, callback: OnPathChangeFunction<T>): this {
+    this.onpathset[path] = this.onpathset[path] || [];
+    this.onpathset[path].push(callback);
+    return this;
+  }
+
+  onChange(callback: OnChangeFunction<T>): this {
+    this.onchangeListeners.push(callback);
+    return this;
+  }
+
+  offChange(callback: OnChangeFunction<T>): this {
+    this.onchangeListeners = this.onchangeListeners.filter((x) => x !== callback);
+    return this;
+  }
+}
