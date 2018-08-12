5b17c885d9e291d07441f75d93863c27c76444b8
SeanJM <seanjmacisaac@gmail.com>
2018-08-12 07:54:06 -0400

    Deprecated

--- build/html/index.js
+++
@@ -1,9 +0,0 @@
-const { el } = require("flatman-server");
-
-module.exports = function () {
-  const page = el("HTML", [
-    el("div", { id: "app" }),
-    el("script", { src: "bundle.js" })
-  ]);
-  page.toFile("frontend/public/index.html");
-}
