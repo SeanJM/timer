5be0ef2d2edb8f866fa5265e3251f447586465bb
SeanJM <seanjmacisaac@gmail.com>
2018-08-18 13:07:47 -0400

    increased hash uniqness

--- packages/generate-hash.ts
+++ packages/generate-hash.ts
@@ -5,7 +5,7 @@ export default function generateHash(n: number = 32): string {
   let str = "";
   while (++i < n) {
     str += Math.random() > 0.6
-      ? r("abcdef")
+      ? r("abcdefghijklmnopqrstuvwxyz")
       : r("0123456789");
   }
   return str;
