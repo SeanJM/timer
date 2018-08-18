962a7224997cca1c8b5abf226b02913f895ba864
SeanJM <seanjmacisaac@gmail.com>
2018-08-18 08:57:24 -0400

    App rendering

---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+import "./index.scss";
+import App from "@components/app";
+import ReactDOM from "react-dom";
+import React from "react";
+
+ReactDOM.render(<App />, document.getElementById("app"));
