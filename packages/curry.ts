1f144065f50bd50ef475c8b340f49c63606df783
SeanJM <seanjmacisaac@gmail.com>
2018-08-18 13:08:47 -0400

    simplified curry function

--- packages/curry.ts
+++ packages/curry.ts
@@ -3,26 +3,10 @@
  * @param {...*} any
  * @returns {function} While the arguments are incomplete, it returns another curried function
  */
-export default function curry(fn: <T>(...args: any[]) => T) {
-  const args = [];
-  let i = 0;
-  const n = arguments.length;
-
-  while (++i < n) {
-    args.push(arguments[i]);
-  }
-
+export default function curry<G, T>(fn: (...args: any[]) => T, ...args: any[]): G {
   return args.length >= fn.length
     ? fn.apply(null, args)
-    : function () {
-      let i = -1;
-      const n = arguments.length;
-      const args2 = [];
-
-      while (++i < n) {
-        args2.push(arguments[i]);
-      }
-
+    : function (...args2: any[]) {
       return curry.apply(null, [fn].concat(args.concat(args2)));
     };
 }
