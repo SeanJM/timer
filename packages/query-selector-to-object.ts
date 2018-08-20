ea788e3d047f048960baba309e56ca8f7a41ee22
SeanJM <seanjmacisaac@gmail.com>
2018-08-20 18:51:41 -0400

    Made query-selector-to-object complete

--- packages/query-selector-to-object.ts
+++ packages/query-selector-to-object.ts
@@ -1,12 +1,16 @@
 import Element, { ElementAttributes } from "@class/element";
 
-export default function querySelectorToObject(selector: string) {
-  const attributes: ElementAttributes = {};
-  const queryObject: Partial<Element> = {
+type QueryObject = Partial<Element>;
+
+export default function querySelectorToObjectList(selector: string) {
+  const queryList: QueryObject[] = [];
+  let attributes: ElementAttributes = {};
+  let queryObject: QueryObject = {
     attributes,
   };
 
   let i = -1;
+  let c;
   const n = selector.length;
 
   const boundary = {
@@ -20,26 +24,71 @@ export default function querySelectorToObject(selector: string) {
   while (++i < n) {
     if (/[a-zA-Z]/.test(selector[i])) {
       queryObject.type = "";
+
       while (selector[i] && !boundary[selector[i]]) {
         queryObject.type += selector[i];
         i += 1;
       }
-    } else if (selector[i] === "#") {
+
+      i -= 1;
+    }
+    
+    if (selector[i] === "#") {
       attributes.id = "";
       i += 1;
+
       while (selector[i] && !boundary[selector[i]]) {
         attributes.id += selector[i];
         i += 1;
       }
-    } else if (selector[i] === ".") {
+
+      i -= 1;
+    }
+    
+    if (selector[i] === ".") {
       attributes.className = attributes.className ? attributes.className + " " : "";
       i += 1;
+
       while (selector[i] && !boundary[selector[i]]) {
         attributes.className += selector[i];
         i += 1;
       }
+
+      i -= 1;
+    }
+    
+    if (selector[i] === "[") {
+      c = [""];
+      i += 1;
+      
+      while (selector[i] && selector[i] !== "]") {
+        c[0] += selector[i];
+        i += 1;
+      }
+      c[1] = c[0].match(/[A-Za-z\-\_]+|/)[0];
+      c[2] = c[0].match(/[\^|\*|\$]=|/)[0][0];
+      c[3] = (c[0].match(/=[^\]]+$/) || [""])[0].substring(1).replace(/^"|"$/g, "");
+      if (c[2] === "^") {
+        c[4] = new RegExp("^" + c[3]);
+      } else if (c[2] === "$") {
+        c[4] = new RegExp(c[3] + "$");
+      } else if (c[2] === "*") {
+        c[4] = new RegExp(c[3]);
+      } else {
+        c[4] = c[3];
+      }
+      attributes[c[0].match(/[A-Za-z\-\_]+|/)[0]] = c[4];
+      i -= 1;
+    }
+
+    if (selector[i] === " " || i === n - 1) {
+      queryList.push(queryObject);
+      attributes = {};
+      queryObject = {
+        attributes,
+      };
     }
   }
-
-  return queryObject;
+  
+  return queryList;
 }
