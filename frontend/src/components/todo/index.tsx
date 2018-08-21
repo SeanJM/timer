44aa253f8985b49e7b46cf21b637d856ea9cc6e0
SeanJM <seanjmacisaac@gmail.com>
2018-08-21 15:09:14 -0400

    Refactored to use "ListItem" and "Timestamp"

--- frontend/src/components/todo/index.tsx
+++ frontend/src/components/todo/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Button from "@components/button";
-import { Link } from "@components/router";
 import Control from "@components/control";
+import { ListItem } from "@components/list";
 import path from "@path";
 import { dispatch } from "@action";
-
-function timestamp(n: number) {
-  const d = new Date(n);
-  const date = d.getFullYear() + "-" + ("0" + d.getMonth()).substr(-2) + "-" + ("0" + d.getDate()).substr(-2);
-  const time = (d.getHours() % 12) + ":" + d.getMinutes() + (d.getHours() > 12 ? "PM" : "AM");
-  return date + " " + time
-}
+import Timestamp from "@components/timestamp";
 
 interface TodoProps {
   state: string;
@@ -69,19 +63,24 @@ export default function Todo(props: TodoProps) {
   const className = ["todo"];
   className.push("todo-" + props.state);
   return (
-    <div className={className.join(" ")}>
-      <h5>{props.name}</h5>
-      <p>{timestamp(props.created)}</p>
-      <Link
-        className="todo_link"
-        to={path.join("/todo/category/", props.categoryID, props.id)}
-      />
-      <Control className="todo_control">
-        {props.state === "complete"
-          ? props.showAlt ? <ButtonUndone id={props.id} categoryID={props.categoryID} /> : <ButtonDelete id={props.id} categoryID={props.categoryID} />
-          : props.showAlt ? <ButtonDelete id={props.id} categoryID={props.categoryID} /> : <ButtonDone id={props.id} categoryID={props.categoryID} />
-        }
-      </Control>
-    </div>
+    <ListItem
+      passive={props.state === "complete"}
+      title={props.name}
+      timestamp={<Timestamp>{props.created}</Timestamp>}
+      to={path.join("/category/", props.categoryID, props.id)}
+      primaryAction={
+        <Control>
+          {props.state === "complete"
+            ? props.showAlt
+              ? <ButtonUndone id={props.id} categoryID={props.categoryID} />
+              : <ButtonDelete id={props.id} categoryID={props.categoryID} />
+            : props.showAlt
+              ? <ButtonDelete id={props.id} categoryID={props.categoryID} />
+              : <ButtonDone id={props.id} categoryID={props.categoryID} />
+          }
+        </Control>
+      }
+    >
+    </ListItem>
   );
 }
