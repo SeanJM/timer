f2134d5e70aa33e87159aea556a23a83e3ae6def
SeanJM <seanjmacisaac@gmail.com>
2018-08-21 13:18:55 -0400

    Added "GET_TAGS" action

--- frontend/src/action/todo-tags/service.ts
+++ frontend/src/action/todo-tags/service.ts
@@ -1,17 +1,26 @@
 import ajax from "@scripts/ajax";
-import { store, TodoTag } from "@store";
+import { store, TagNode } from "@store";
 import path from "@path";
 
 export default class Service {
+  get(e) {
+    ajax.get(path.join("/tags/", e.categoryID))
+      .then(function (tags: TagNode[]) {
+        store.set({
+          tags,
+        });
+      });
+  }
+
   createTag(e) {
     ajax.post(path.join("/todo/tags/", e.categoryID), {
       data: {
         action: "create",
         colorID: e.colorID,
         name: e.name,
-      } as Pick<TodoTag, "colorID" | "name">
+      } as Pick<TagNode, "colorID" | "name">
     })
-      .then(function (tag: TodoTag) {
+      .then(function (tag: TagNode) {
         store.set({
           tags: store.value.tags.concat(tag)
         });
