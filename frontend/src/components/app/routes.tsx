af1125b94d2985070cf37b0ef1109978e8cb66b9
SeanJM <seanjmacisaac@gmail.com>
2018-08-25 12:55:40 -0400

    renamed "category" to "pathname"

--- frontend/src/components/app/routes.tsx
+++ frontend/src/components/app/routes.tsx
@@ -1,11 +1,11 @@
 import path from "@path";
-export const category = "/:type/:categoryID";
+export const pathname = "/:type/:categoryID";
 export const root = "/:type/";
 
 export const tagsRoot = path.replace(root, { type: "tags" });
 export const todoRoot = path.replace(root, { type: "todo" });
 
 export const params = {
-  todoCategory: path.replace(category, { type: "todo" }),
-  tagsCategory: path.replace(category, { type: "tags" }),
+  todoCategory: path.replace(pathname, { type: "todo" }),
+  tagsCategory: path.replace(pathname, { type: "tags" }),
 };
