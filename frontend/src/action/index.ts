2159357136b07bd34cd8a81be1cf643dcdd5f447
SeanJM <seanjmacisaac@gmail.com>
2018-08-25 12:45:02 -0400

    Added action initializers for color

--- frontend/src/action/index.ts
+++ frontend/src/action/index.ts
@@ -1,10 +1,11 @@
 import Action from "@class/action";
-import modal from "@action/modal";
+import color from "@action/color";
 import form from "@action/form";
+import keydown from "@action/keydown";
+import modal from "@action/modal";
 import slideOut from "@action/slide-out";
 import todo from "@action/todo";
 import todoTags from "@action/todo-tags";
-import keydown from "@action/keydown";
 
 const action = new Action();
 action.debug = process.env.NODE_ENV === "development";
@@ -15,9 +16,10 @@ export function dispatch(name: string, value?: any): void {
 
 export const subscribe = (name, callback) => action.subscribe(name, callback);
 
-modal(subscribe);
+color(subscribe);
 form(subscribe);
+keydown(subscribe);
+modal(subscribe);
+slideOut(subscribe);
 todo(subscribe);
 todoTags(subscribe);
-slideOut(subscribe);
-keydown(subscribe);
