ce4452feba0394343772a29976b8e5ef0e1401c9
SeanJM <seanjmacisaac@gmail.com>
2018-08-25 12:44:13 -0400

    actions for interacting with color

---
+++ frontend/src/action/color.ts
@@ -0,0 +1,142 @@
+import { store, ColorPicker } from "@store";
+import { SwatchAttributes } from '@types';
+import ajax from "@ajax";
+import _ from "lodash";
+import generateHash from "@generate-hash";
+
+function swatchToAttributes(value: string): SwatchAttributes {
+  return {
+    created: new Date().getTime(),
+    value: value.substring(1), // Remove the hash
+    id: generateHash(),
+  };
+}
+
+function attributesToSwatch(swatch: SwatchAttributes): SwatchAttributes {
+  return {
+    created: swatch.created,
+    value: "#" + swatch.value, // Add the hash
+    id: swatch.id,
+  };
+}
+
+function addToPalette(value: string) {
+  const swatch = swatchToAttributes(value);
+  const indexOf = store.value.color.palette.findIndex(a => a.value === value);
+
+  if (indexOf === -1) {
+    store.set({
+      color: {
+        palette: store.value.color.palette.concat(swatch),
+      }
+    });
+
+    ajax.post("/color/palette/", {
+      data: {
+        action: "create",
+        value: swatch.value,
+      }
+    }).then((res: SwatchAttributes) => {
+      const palette = store.value.color.palette.slice();
+      const indexOf = palette.findIndex(a => a.id === swatch.id);
+
+      palette.splice(indexOf, 1, {...res, value: "#" + res.value});
+
+      store.set({
+        color: {
+          palette,
+        }
+      });
+    });
+  }
+}
+
+function getPalette() {
+  ajax.get("/color/palette/").then((palette: SwatchAttributes[]) => {
+    store.set({
+      color: {
+        palette: palette.map(attributesToSwatch),
+      }
+    });
+  });
+}
+
+function deleteSwatch(id: string) {
+  const swatchIndex = store.value.color.palette.findIndex(a => a.id === id);
+  const swatch = store.value.color.palette[swatchIndex];
+  const palette = store.value.color.palette.filter(a => a.id !== id);
+
+  store.set({
+    color: {
+      palette,
+    }
+  });
+
+  ajax.post("/color/palette/", {
+    data: {
+      action: "delete",
+      id: id,
+    }
+  })
+    .catch(() => {
+      const undoPalette = palette.slice();
+      undoPalette.splice(swatchIndex, 0, swatch);
+      store.set({
+        color: {
+          palette: undoPalette,
+        }
+      });
+    });
+}
+
+export default function (subscribe) {
+  subscribe("COLOR", function ({ type, value, id }) {
+    if (type === "ADD_TO_PALETTE") {
+      addToPalette(value);
+    } else if (type === "GET_PALETTE") {
+      getPalette();
+    } else if (type === "DELETE_SWATCH") {
+      deleteSwatch(id);
+    }
+  });
+
+  subscribe("COLOR_PICKER", function (e: { type: string, value?: string; id: string }) {
+    const items: ColorPicker[] = _.merge([], store.value.color.items);
+    const previousItem = items.find((a) => a.id === e.id);
+
+    if (e.type === "OPEN") {
+      if (previousItem) {
+        previousItem.isOpen = true;
+        previousItem.value =
+          typeof e.value === "string"
+            ? e.value
+            : previousItem.value;
+      } else {
+        items.push({
+          value: e.value || "#c18a8a",
+          id: e.id,
+          isOpen: true,
+        });
+      }
+      store.set({
+        color: {
+          items,
+        }
+      });
+    }
+  });
+
+  subscribe("COLOR_PICKER", function (e: { type: string, id: string, value: string }) {
+    if (e.type === "CLOSE") {
+      const items = _.merge([], store.value.color.items);
+      const item: ColorPicker = items.find((a) => a.id === e.id);
+      item.isOpen = false;
+      item.value = typeof e.value === "string" ? e.value : item.value;
+      store.set({
+        color: {
+          items,
+        }
+      });
+    }
+  });
+}
