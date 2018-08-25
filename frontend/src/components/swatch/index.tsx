ee9e916a3c9625809c72a51cdfb3ebce2abdf0b5
SeanJM <seanjmacisaac@gmail.com>
2018-08-25 13:01:54 -0400

    simplified

--- frontend/src/components/swatch/index.tsx
+++ frontend/src/components/swatch/index.tsx
@@ -1,87 +1,32 @@
-import React, { Component } from "react";
-import rand from "@rand";
-import colors from "@components/swatch/colors";
-import Swatch from "@components/swatch/swatch";
-import Picker from "@components/swatch/picker";
+import React from "react";
 
-interface State {
-  showPicker: boolean;
-  color: null | string;
+export interface SwatchProps extends Partial<JSX.ElementChildrenAttribute> {
+  onClick?: () => void;
+  background?: string;
+  isSelected?: boolean;
+  className?: string;
 }
 
-interface SelectEvent {
-  color: string;
-  index: number;
-}
+export default function Swatch(props: SwatchProps) {
+  const className = ["swatch"];
 
-interface SwatchPickerProps {
-  onSelect: (e: SelectEvent) => void;
-}
-
-export default class SwatchPicker extends Component<SwatchPickerProps, State> {
-  node: HTMLDivElement;
-
-  onClick() {
-    if (this.state.showPicker) {
-      setTimeout(() => {
-        this.setState({
-          showPicker: false
-        });
-      }, 150);
-    }
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPicker: false,
-      color: rand(colors),
-    };
-    this.onClick = this.onClick.bind(this);
+  if (props.isSelected) {
+    className.push("swatch-select");
   }
 
-  onSelect(color: string) {
-    const { onSelect } = this.props;
-    this.setState({
-      color,
-    });
-    onSelect && onSelect({
-      color,
-      index: colors.indexOf(color)
-    });
+  if (props.className) {
+    className.push(props.className);
   }
 
-  componentDidMount() {
-    const { onSelect } = this.props;
-    document.body.addEventListener("click", this.onClick);
-    onSelect && onSelect({
-      color: this.state.color,
-      index: colors.indexOf(this.state.color),
-    });
-  }
-  
-  componentWillUnmount() {
-    document.body.removeEventListener("click", this.onClick);
-  }
-
-  render() {
-    return (
-      <div ref={(node) => { this.node = node; }} className="swatch-picker">
-        <Swatch background={this.state.color} onClick={() => {
-          this.setState({
-            showPicker: true,
-          })
-        }}>
-          {this.state.showPicker 
-            ? (
-              <Picker
-                selectedColor={this.state.color}
-                onSelect={(color) => this.onSelect(color)}
-              />
-            )
-            : null}
-        </Swatch>
-      </div>
-    );
-  }
+  return (
+    <div
+      onClick={props.onClick}
+      className={className.join(" ")}
+    >
+      <div style={{background: props.background}} className="swatch_color"></div>
+      <div className="swatch_face"></div>
+      <div className="swatch_select"></div>
+      {props.children}
+    </div>
+  );
 }
