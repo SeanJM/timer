f6fde620ff8474897f00f867599ee55ffedd75a8
SeanJM <seanjmacisaac@gmail.com>
2018-08-25 22:17:10 -0400

    Added more types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,39 @@
+import Element from "@backend/class/element";
+
+export interface TodoElement extends
+  Element {
+  type: "todo";
+  attributes: Pick<Element["attributes"], "id" | "created" | "name" | "state">;
+  children: any[];
+}
+
+export type TodoResponse =
+  Pick<Element["attributes"],
+    | "id"
+    | "created"
+    | "name"
+    | "state"
+  >;
+
+export type CategoryElementAttributes =
+  Pick<Element["attributes"],
+  | "id"
+  | "created"
+  | "name"
+  >;
+
+export interface CategoryElement extends
+  Element {
+  type: "category";
+  attributes: CategoryElementAttributes;
+  children: TodoElement[];
+}
+
+export interface CategoryResponse extends
+  Pick<CategoryElementAttributes, "id" | "created" | "name"> {
+  todos: TodoResponse[];
+}
+
 export interface SwatchAttributes {
   value: string;
   created: number;
