e4b348efd80cf2f56636543f6698f6b7d9daba03
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-08-31 09:48:45 -0400

    Checkbox

---
+++ frontend/src/components/checkbox/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Icon from "@frontend/components/icon";
+
+interface CheckboxProps {
+  checked: boolean;
+  fill?: string;
+}
+
+export function Checkbox(props: CheckboxProps) {
+  const className = ["checkbox"];
+  if (props.checked) {
+    className.push("checkbox--checked");
+  }
+  return (
+    <div className={className.join(" ")}>
+      {props.checked ? <Icon type="check" fill={props.fill}/> : null}
+    </div>
+  );
+}
