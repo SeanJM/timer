8e2a22b53cf4142431df0797d336c2ad1d171c34
SeanJM <seanjmacisaac@gmail.com>
2018-08-31 08:07:53 -0400

    renamed "TagNode" to "TagElement"

--- frontend/src/action/todo-tags/service.ts
+++ frontend/src/action/todo-tags/service.ts
@@ -1,6 +1,6 @@
 import ajax from "@ajax";
 import { store } from "@frontend/store";
-import { TagNode, TagCategory } from "@types";
+import { TagResponse, TagCategory } from "@types";
 import { replaceById } from "@replace-by-id";
 import path from "@path";
 import generateHash from "@generate-hash";
@@ -9,7 +9,7 @@ import _ from "lodash";
 export default class Service {
   get(e) {
     ajax.get(path.join("/tags/", e.categoryID))
-      .then(function (tags: TagNode[]) {
+      .then(function (tags: TagResponse[]) {
         const categories: TagCategory[] = _.merge([], store.value.tags.categories);
         const category = categories.find(a => a.id === e.categoryID);
         const mappedTags = tags.map(tag => ({ ...tag, color: tag.color ? "#" + tag.color : null }));
@@ -35,7 +35,7 @@ export default class Service {
     const categories: TagCategory[] = _.merge([], store.value.tags.categories);
     const category = categories.find(a => a.id === e.categoryID);
 
-    const nextTag: TagNode = {
+    const nextTag: TagResponse = {
       created: new Date().getTime(),
       name: e.name,
       color: e.color,
@@ -55,9 +55,9 @@ export default class Service {
         action: "create",
         color: e.color.substring(1),
         name: e.name,
-      } as Pick<TagNode, "color" | "name">
+      } as Pick<TagResponse, "color" | "name">
     })
-      .then(function (tag: TagNode) {
+      .then(function (tag: TagResponse) {
         const categories: TagCategory[] = _.merge([], store.value.tags.categories);
         const category = categories.find(a => a.id === e.categoryID);
         replaceById(category.tags, { ...tag, color: "#" + tag.color }, nextTag);
