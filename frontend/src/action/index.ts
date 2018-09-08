1fc96f427ee01d5f86cda1ebb971f4f230b02a5e
SeanJM <seanjmacisaac@gmail.com>
2018-09-08 08:00:17 -0400

    Removed default form export

--- frontend/src/action/index.ts
+++ frontend/src/action/index.ts
@@ -1,7 +1,7 @@
 import Action from "@frontend/class/action";
 import category from "@frontend/action/category";
 import color from "@frontend/action/color";
-import form from "@frontend/action/form";
+import { form } from "@frontend/action/form";
 import keydown from "@frontend/action/keydown";
 import modal from "@frontend/action/modal";
 import slideOut from "@frontend/action/slide-out";
