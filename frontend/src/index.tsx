863ae15d68aaa88875cc6a7b40bf7a086d7a2f4e
SeanJM <seanjmacisaac@gmail.com>
2018-09-08 07:59:05 -0400

    Removed app default export

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import "./index.scss";
-import App from "@frontend/components/app";
+import { App } from "@frontend/components/app";
 import ReactDOM from "react-dom";
 import React from "react";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
