2065f8ed9defcbd54fa81a86874dff92c4a905e5
SeanJM <seanjmacisaac@gmail.com>
2018-09-09 11:39:44 -0400

    Removed router and class setting from app

--- frontend/src/components/app/index.tsx
+++ frontend/src/components/app/index.tsx
@@ -1,53 +1,44 @@
-import * as React from "react";
+import React, { Component } from "react";
 import { TypeSelectionConnect } from "@frontend/pages/type-menu";
-import { Router, Route, WithRouterProps, withRouter } from "@frontend/components/router";
+import { Router, Route } from "@frontend/components/router";
 import { ModalConnect } from "@frontend/components/modal";
 import { SlideOutContainerConnect } from "@frontend/containers/slide-out";
 import { ColorPickerSpawnConnect } from "@frontend/components/color-picker";
 import { dispatch } from "@frontend/action/";
-import TodoList from "@frontend/pages/todo-list";
-import TodoTags from "@frontend/pages/todo-tags";
+import { TodoList } from "@frontend/pages/todo-list";
+import { TodoTags } from "@frontend/pages/todo-tags";
 import { routes } from "@frontend/routes";
 import path from "@path";
 import { CategoryListConnect } from "@frontend/pages/category-list";
 import { TodoEditorConnect } from "@frontend/pages/todo-editor";
 import { NotFound } from "@frontend/pages/not-found";
 
-class AppView extends React.Component<WithRouterProps> {
+export class App extends Component {
+  handleEvent(e: KeyboardEvent) {
+    switch (e.type) {
+      case "keydown": {
+        if (e.which === 91) {
+          dispatch("KEYDOWN_CTRL");
+        }
+        break;
+      }
+
+      case "keyup": {
+        if (e.which === 91) {
+          dispatch("KEYUP_CTRL");
+        }
+      }
+    }
+  }
+
   componentDidMount() {
-    dispatch("CATEGORY", {
-      type: "GET_ALL",
-    });
-
-    document.addEventListener("keydown", function (e) {
-      if (e.which === 91) {
-        dispatch("KEYDOWN_CTRL");
-      }
-    });
-
-    document.addEventListener("keyup", function (e) {
-      if (e.which === 91) {
-        dispatch("KEYUP_CTRL");
-      }
-    });
+    dispatch("CATEGORY", { type: "GET_ALL" });
+    document.addEventListener("keydown", this);
+    document.addEventListener("keyup", this);
   }
 
   render() {
-    const params = path.params(this.props.location.pathname, routes.pathname);
     const className = ["app"];
-
-    if (params.type) {
-      className.push("app-type");
-    }
-
-    if (params.categoryID) {
-      className.push("app-category-id");
-    }
-
-    if (params.todoID) {
-      className.push("app-todo-id");
-    }
-
     return (
       <div className={className.join(" ")}>
         <Router>
@@ -78,5 +69,4 @@ class AppView extends React.Component<WithRouterProps> {
   }
 }
 
-export const App = withRouter<{}>(AppView);
 export { routes };
