7b225ed650c0a1b3f68360c675b9efc45b68362d
SeanJM <seanjmacisaac@gmail.com>
2018-09-09 21:18:05 -0400

    Minor perf change to remove dependency on parsing

--- packages/path/join.ts
+++ packages/path/join.ts
@@ -1,14 +1,15 @@
-import parse from "@path/parse";
 import normalize from "@path/normalize";
 export default function join(...pathname: string[]) {
   let i = -1;
-  const p = parse(pathname[0]);
+  const root = pathname[0][0] === "/" ? "/" : "";
   const n = pathname.length;
   const res = [];
 
   while (++i < n) {
-    res.push(normalize(pathname[i]));
+    if (pathname[i] !== "/") {
+      res.push(normalize(pathname[i]));
+    }
   }
 
-  return p.root + res.join("/") + "/";
+  return root + res.join("/") + "/";
 }
