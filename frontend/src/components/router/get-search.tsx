723efdc51a055c72f677f05b964bd298fb739d6f
SeanJM <seanjmacisaac@gmail.com>
2018-09-09 21:14:43 -0400

    "getSearch" requires the "?" prefix

--- frontend/src/components/router/get-search.tsx
+++ frontend/src/components/router/get-search.tsx
@@ -1,5 +1,5 @@
-export default function getSearch(url: string): string | null {
-  const start = url.indexOf("?") > -1 ? url.indexOf("?") : 0;
-  const end = url.indexOf("#") > -1 ? url.indexOf("#") : url.length;
-  return url.substring(start, end) || null;
+export default function getSearch(url: string): string {
+  return url.indexOf("?") === -1
+    ? ""
+    : url.split("?")[1].split("#")[0];
 }
