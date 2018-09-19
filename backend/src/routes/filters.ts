1828c07aec36d132dcafed1916fb875c26538c7f
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-09-19 09:48:46 -0400

    Filter end point

---
+++ backend/src/routes/filters.ts
@@ -0,0 +1,114 @@
+import { Response, Express } from "express";
+import Validate from "verified";
+import { Database } from "@backend/class/database";
+import generateHash from "@generate-hash";
+import { FilterResponse, FilterElement, CategoryElement } from "@types";
+import path, { PathParams } from "@path";
+
+interface FilterGetRequest {
+  params: PathParams<{
+    categoryID: string
+  }>;
+}
+
+interface FilterPostRequest {
+  query: {
+    action: "create" | "delete" | "edit",
+    name?: string,
+    tags?: string[],
+  };
+  params: PathParams<{ categoryID: string, filterID: string }>;
+}
+
+export function toFilterResponse(element: FilterElement): FilterResponse {
+  return {
+    name: element.attributes.name,
+    id: element.attributes.id,
+    created: element.attributes.created,
+    tags: element.attributes.tags,
+  };
+}
+
+function createFilter(req: FilterPostRequest, res: Response, database: Database) {
+  const { categoryID } = req.params;
+  const category = database.getElementById(categoryID);
+
+  const filterElement = database.createElement<FilterElement>("filter", {
+    name: req.query.name,
+    created: new Date().getTime(),
+    id: generateHash(7),
+    tags: []
+  });
+
+  if (category) {
+    category.appendChild(filterElement);
+    res.send(toFilterResponse(filterElement));
+    database.save();
+  } else {
+    res
+      .status(404)
+      .send(`FILTERS__INVALID_CATEGORY_ID: "${categoryID}"`);
+  }
+}
+
+function deleteFilter(req, res, database) {
+
+}
+
+function editFilter(req: FilterPostRequest, res, database) {
+  const { categoryID, filterID } = req.params;
+  const category: CategoryElement = database.getElementById(categoryID);
+  const filterElement = category.querySelector<FilterElement>(`#${filterID}`);
+  if (category && filterElement) {
+    filterElement.setAttributes({
+      name: req.query.name || filterElement.attributes.name,
+      tags: req.query.tags || filterElement.attributes.tags,
+    });
+
+    res.send(toFilterResponse(filterElement));
+    database.save();
+  } else if (!filterElement) {
+    res.status(404).send(`FILTERS__FILTER_ID_NOT_FOUND: "${filterID}"`);
+  } else {
+    res.status(404).send(`FILTERS__CATEGORY_ID_NOT_FOUND: "${categoryID}"`);
+  }
+}
+
+function onPost(req: FilterPostRequest, res: Response, database) {
+  const validateQuery = new Validate({
+    action: "create|delete|edit",
+    "name?": "string",
+    "tags?": "string[]",
+  });
+
+  if (validateQuery.validate(req.query).isValid) {
+    if (req.query.action === "create") {
+      createFilter(req, res, database);
+    } else if (req.query.action === "delete") {
+      deleteFilter(req, res, database);
+    } else if (req.query.action === "edit") {
+      editFilter(req, res, database);
+    }
+  } else {
+    res.status(500).send("TAG__INVALID_REQUEST");
+  }
+}
+
+function onGet(req: FilterGetRequest, res: Response, database) {
+  const filterElements: FilterResponse[] = database.body
+    .querySelectorAll(`#${req.params.categoryID} filter`)
+    .map(toFilterResponse);
+  res.send(filterElements);
+}
+
+export function filters(database: Database, app: Express) {
+  app.use("/filters", function (req, res, next) {
+    req.params = path(req.url).params("/:categoryID/:filterID");
+    if (req.method === "POST") {
+      onPost(req, res, database);
+    } else if (req.method === "GET") {
+      onGet(req, res, database);
+    }
+    next();
+  });
+}
