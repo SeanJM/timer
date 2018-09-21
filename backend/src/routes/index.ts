27f0776c350dee94548f885aeac8fa7b1723d1aa
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-09-21 12:20:38 -0400

    Added "filters" end point and removed default exports

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Express } from "express";
-import todo from "@backend/routes/todo";
-import category from "@backend/routes/category";
+import { todo } from "@backend/routes/todo";
+import { category } from "@backend/routes/category";
 import { tagsRoute } from "@backend/routes/tags";
-import color from "@backend/routes/color";
+import { filters } from "@backend/routes/filters";
+import { color } from "@backend/routes/color";
 import { Database } from "@backend/class/database";
 
 export function routes(app: Express, database: Database) {
   app.use("/category", category(database, app));
   app.use("/todo", todo(database, app));
   app.use("/tags", tagsRoute(database, app));
+  filters(database, app);
   app.use("/color", color(database));
 }
