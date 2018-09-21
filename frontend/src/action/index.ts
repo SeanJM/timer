0dba17ce1a36eca6b7fd265c738f7d61e7484ec3
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-09-21 12:24:22 -0400

    Removed "slide-out"

--- frontend/src/action/index.ts
+++ frontend/src/action/index.ts
@@ -1,12 +1,13 @@
 import Action from "@frontend/class/action";
-import category from "@frontend/action/category";
+import { category } from "@frontend/action/category";
+import { contextMenu } from "@frontend/action/context-menu";
 import color from "@frontend/action/color";
 import { form } from "@frontend/action/form";
 import keydown from "@frontend/action/keydown";
 import modal from "@frontend/action/modal";
-import slideOut from "@frontend/action/slide-out";
 import todo from "@frontend/action/todo";
 import { tags } from "@frontend/action/tags";
+import { filters } from "@frontend/action/filters";
 
 const action = new Action();
 action.debug = process.env.NODE_ENV === "development";
@@ -15,13 +16,16 @@ export function dispatch(name: string, value?: any): void {
   action.dispatch(name, value);
 }
 
-export const subscribe = (name, callback) => action.subscribe(name, callback);
+export function subscribe(name, callback): void {
+  action.subscribe(name, callback);
+}
 
 category(subscribe);
 color(subscribe);
+contextMenu(subscribe);
+filters(subscribe);
 form(subscribe);
 keydown(subscribe);
 modal(subscribe);
-slideOut(subscribe);
-todo(subscribe);
 tags(subscribe);
+todo(subscribe);
