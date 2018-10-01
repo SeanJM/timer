4fc4a472b56fc191ea09fd1d26aad53fcc3f3d1b
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-01 09:36:33 -0400

    made "generateHash" function more variable

--- packages/generate-hash.ts
+++ packages/generate-hash.ts
@@ -1,11 +1,12 @@
-function r(s) { return s[Math.round(Math.random() * (s.length - 1))]; }
+const { round, random } = Math;
+const r = (s) => s[round(random() * (s.length - 1))];
 
 export default function generateHash(n: number = 32): string {
   let i = -1;
   let str = "";
   while (++i < n) {
-    str += Math.random() > 0.6
-      ? r("abcdefghijklmnopqrstuvwxyz")
+    str += random() > 0.6
+      ? r("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
       : r("0123456789");
   }
   return str;
