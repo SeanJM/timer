75d9fc919e9c6690be649ef19f4fe9646000e650
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-02 10:17:36 -0400

    Added UI for various filters

--- frontend/src/pages/filters/filter-editor.tsx
+++ frontend/src/pages/filters/filter-editor.tsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
-import { InputChipSelect } from "@frontend/components/input";
-import { WithRouterProps } from "@frontend/components/router";
 import generateHash from "@generate-hash";
-import { Viewport } from "@frontend/components/viewport";
-import { withStore, StoreState } from "@frontend/store";
-import { TagResponse } from "@types";
-import { PathParams } from "@path";
-import { FormConnect, InputGroup } from "@frontend/components";
-import { ChipData } from "@frontend/components/chip";
-import { emptyForm } from "@frontend/action/form";
+
+import { Button } from "@components/button";
+import { ChipData } from "@components/chip";
 import { dispatch } from "@frontend/action";
+import { FormConnect } from "@components/form";
+import { InputChipSelect } from "@components/input";
+import { InputGroup } from "@components/input-group";
+import { InputText } from "@components/input";
+import { PathParams } from "@path";
+import { TagResponse, FilterTagTypes } from "@types";
+import { TitleAndInput } from "@components/title-and-input";
+import { Titlebar } from "@components/titlebar";
+import { Viewport } from "@components/viewport";
+import { WithRouterProps } from "@components/router";
+import { withStore, StoreState } from "@frontend/store";
+
+import { path } from "@path";
 
 const FORM_ID = generateHash();
 
@@ -21,13 +28,17 @@ interface FilterEditorInProps extends WithRouterProps {
   }>;
 }
 
-interface FilterEditorOutProps {
-  filterTags: string[];
+interface FilterEditorOutProps extends Pick<WithRouterProps, "history"> {
+  tagFilters: {
+    [key in FilterTagTypes]: string[];
+  };
   filterID: string;
   categoryID: string;
   categoryTags: TagResponse[];
+  filterName: string;
   form: {
-    tags: string[]
+    tagFilters: { [key in FilterTagTypes]: string[] };
+    filterName: string;
   };
 }
 
@@ -36,15 +47,23 @@ function mapStateToProps(state: StoreState, props: FilterEditorInProps): FilterE
   const filterID = props.params.filterID;
   const category = state.todo.categories.find((a) => a.id === categoryID);
   const filter = category.filters.find((a) => a.id === filterID);
-  const form = state.form[FORM_ID] || emptyForm(FORM_ID, ["filterTags"]);
+  const form = state.form[FORM_ID];
+
   return {
+    history: props.history,
     categoryID,
     filterID,
+    filterName: filter.name,
     form: {
-      tags: form.input.filterTags.value || [],
+      tagFilters: {
+        includes: form ? (form.input.includesTags || { value: [] }).value : [],
+        excludes: form ? (form.input.excludesTags || { value: [] }).value : [],
+        any: form ? (form.input.containsAnyTag || { value: [] }).value : [],
+      },
+      filterName: form ? (form.input.filterName || { value: "" }).value : "",
     },
+    tagFilters: filter.tagFilters,
     categoryTags: category.tags,
-    filterTags: filter.tags,
   };
 }
 
@@ -55,36 +74,136 @@ export class FilterEditorView extends Component<FilterEditorOutProps> {
       value: {
         categoryID: this.props.categoryID,
         filterID: this.props.filterID,
-        tags: this.props.form.tags,
+        tagFilters: this.props.form.tagFilters,
+      }
+    });
+  }
+
+  nameDidChange() {
+    dispatch("FILTERS", {
+      type: "EDIT",
+      value: {
+        categoryID: this.props.categoryID,
+        filterID: this.props.filterID,
+        name: this.props.form.filterName,
       }
     });
   }
 
   componentDidUpdate(prevProps: FilterEditorOutProps) {
     const isFilterID = this.props.filterID === prevProps.filterID;
-    const tagsDidChange = this.props.form.tags.length !== prevProps.form.tags.length;
+
+    const nextTags = this.props.form.tagFilters;
+    const prevTags = prevProps.form.tagFilters;
+    const tagsDidChange =
+      nextTags.includes.length !== prevTags.includes.length ||
+      nextTags.excludes.length !== prevTags.excludes.length ||
+      nextTags.any.length !== prevTags.any.length;
+
     if (isFilterID && tagsDidChange) {
       this.tagsDidChange();
     }
   }
 
   render() {
-    const { categoryTags, filterTags } = this.props;
+    const { categoryTags, tagFilters, history } = this.props;
     return (
       <Viewport
+        titlebar={
+          <Titlebar
+            primaryAction={
+              <Button
+                onClick={() => {
+                  history.push({
+                    pathname: path(location.pathname).pop().value
+                  });
+                }}
+                icon="close"
+              />
+            }
+          >
+            <TitleAndInput
+              icon="edit"
+              name="filterName"
+              defaultValue={this.props.filterName}
+              title={this.props.form.filterName || this.props.filterName}
+              component={InputText}
+              onSubmit={() => this.nameDidChange()}
+              onValue={(e) => {
+                dispatch("FORM_VALUE", {
+                  id: FORM_ID,
+                  ...e,
+                });
+            }}
+            />
+          </Titlebar>
+        }
         body={
           <FormConnect type="borderless" id={FORM_ID}>
-            <InputGroup name="filterTags">
-              <label>Tags</label>
+            <InputGroup>
+              <label>Includes tags</label>
               <InputChipSelect
-                defaultValue={filterTags}
-                data={categoryTags.map((tag) => {
-                  return {
-                    id: tag.id,
-                    label: tag.name,
-                    color: tag.color,
-                  } as ChipData;
-                })}
+                name="includesTags"
+                defaultValue={tagFilters.includes}
+                data={categoryTags
+                  .filter((tag) => {
+                    return (
+                      tagFilters.excludes.indexOf(tag.id) === -1 &&
+                      tagFilters.any.indexOf(tag.id) === -1
+                    );
+                  })
+                  .map((tag) => {
+                    return {
+                      id: tag.id,
+                      label: tag.name,
+                      color: tag.color,
+                    } as ChipData;
+                  })
+                }
+              />
+            </InputGroup>
+            <InputGroup>
+              <label>Excludes tags</label>
+              <InputChipSelect
+                name="excludesTags"
+                defaultValue={tagFilters.excludes}
+                data={categoryTags
+                  .filter((tag) => {
+                    return (
+                      tagFilters.includes.indexOf(tag.id) === -1 &&
+                      tagFilters.any.indexOf(tag.id) === -1
+                    );
+                  })
+                  .map((tag) => {
+                    return {
+                      id: tag.id,
+                      label: tag.name,
+                      color: tag.color,
+                    } as ChipData;
+                  })
+                }
+              />
+            </InputGroup>
+            <InputGroup>
+              <label>Contains any tag</label>
+              <InputChipSelect
+                name="containsAnyTag"
+                defaultValue={tagFilters.any}
+                data={categoryTags
+                  .filter((tag) => {
+                    return (
+                      tagFilters.includes.indexOf(tag.id) === -1 &&
+                      tagFilters.excludes.indexOf(tag.id) === -1
+                    );
+                  })
+                  .map((tag) => {
+                    return {
+                      id: tag.id,
+                      label: tag.name,
+                      color: tag.color,
+                    } as ChipData;
+                  })
+                }
               />
             </InputGroup>
           </FormConnect>
