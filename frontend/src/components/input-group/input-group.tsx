85e16b7a97a7530915048f92f0e38ed311359200
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-02 07:29:38 -0400

    Made "input-group" easier to trace

---
+++ frontend/src/components/input-group/input-group.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { Label } from "@frontend/components/label";
+import { InputValueEvent } from "@types";
+
+export interface InputGroupProps extends JSX.ElementChildrenAttribute {
+  formid?: string;
+  required?: boolean;
+}
+
+export function InputGroup(props: InputGroupProps) {
+  const { formid, required } = props;
+  const children = React.Children.toArray(props.children) as JSX.Element[];
+  const className = [
+    "input-group",
+  ];
+
+  return (
+    <div className={className.join(" ")}>
+      {
+        children.map((child) => {
+          if (child.type === Label || child.type === "label") {
+            return React.cloneElement(child, {
+              required,
+            });
+          }
+          return React.cloneElement(child, {
+            formid,
+            required,
+            onValue: (e: InputValueEvent) => {
+              const { onValue } = child.props;
+              if (onValue) {
+                onValue(e);
+              }
+            }
+          });
+        })
+      }
+    </div>
+  );
+}
