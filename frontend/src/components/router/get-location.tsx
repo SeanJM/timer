f6af8881974de458f4db71ba2b9d583ade8548ad
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-02 10:17:09 -0400

    Added "query"

--- frontend/src/components/router/get-location.tsx
+++ frontend/src/components/router/get-location.tsx
@@ -1,12 +1,15 @@
 import getPathname from "@frontend/components/router/get-pathname";
 import getSearch from "@frontend/components/router/get-search";
 import getHash from "@frontend/components/router/get-hash";
+import path from "@path";
 
 export default function getLocation(href: string) {
+  const search = getSearch(href);
   return {
-    href: href,
-    pathname: getPathname(href),
-    search: getSearch(href),
     hash: getHash(href),
+    pathname: getPathname(href),
+    query: path.query(search).value,
+    href: href,
+    search,
   };
 }
