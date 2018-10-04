9dd7f335e767e55984d0dbf30b270aa21aa07a67
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-04 09:00:45 -0400

    "todo-not-found"

---
+++ frontend/src/pages/todo/todo-not-found/todo-not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function TodoNotFound(props) {
+  return (
+    <div className="container">
+      <h5>Error</h5>
+      <h6>The requested todo does not exist</h6>
+    </div>
+  );
+}
