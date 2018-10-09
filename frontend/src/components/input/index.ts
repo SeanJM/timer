6311e47131db9fdbf4554753f3fd63beb11dcea5
SeanJM <seanjmacisaac@gmail.com>
2018-10-09 08:36:26 -0700

    Added missing exports for  "input-slide"

--- frontend/src/components/input/index.ts
+++ frontend/src/components/input/index.ts
@@ -1,4 +1,5 @@
-export * from "@frontend/components/input/input";
+export * from "@frontend/components/input/input-chip-select";
+export * from "@frontend/components/input/input-slide";
 export * from "@frontend/components/input/input-text";
 export * from "@frontend/components/input/input-wrapper";
-export * from "@frontend/components/input/input-chip-select";
+export * from "@frontend/components/input/input";
