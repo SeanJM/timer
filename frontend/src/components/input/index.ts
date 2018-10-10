50113d7f519e87678ad1a9718f3f5bdb3504f82f
SeanJM <seanjmacisaac@gmail.com>
2018-10-10 09:25:41 -0700

    Removed "input-wrapper" export

--- frontend/src/components/input/index.ts
+++ frontend/src/components/input/index.ts
@@ -1,5 +1,4 @@
 export * from "@frontend/components/input/input-chip-select";
 export * from "@frontend/components/input/input-slide";
 export * from "@frontend/components/input/input-text";
-export * from "@frontend/components/input/input-wrapper";
 export * from "@frontend/components/input/input";
