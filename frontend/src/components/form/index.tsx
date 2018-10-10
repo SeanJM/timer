e102afdaf60496285c9fd18d7e621be5fe13c8e2
SeanJM <seanjmacisaac@gmail.com>
2018-10-10 09:04:51 -0700

    Form no longer updates state

--- frontend/src/components/form/index.tsx
+++ frontend/src/components/form/index.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
-import { withStore } from "@frontend/store";
-import { dispatch } from "@frontend/action";
+import { InputValueEvent } from "@types";
 
-function mapStateToProps(state, props) {
-  return {
-    ...state.form[props.id],
-    ...props,
-  };
+export type FormValue<T = {}> = Partial<T> & {
+  [key: string]: InputValueEvent["value"];
+};
+
+interface FormChangeEvent {
+  type: string;
+  value: FormValue;
+}
+
+interface FormOnChangeFunction {
+  (e: FormChangeEvent): void;
 }
 
 interface FormProps extends JSX.ElementChildrenAttribute {
@@ -14,51 +19,78 @@ interface FormProps extends JSX.ElementChildrenAttribute {
   type?: "borderless";
   showValidation?: boolean;
   onSubmit?: (e: React.FormEvent) => void;
+  onChange?: FormOnChangeFunction | { handleEvent: FormOnChangeFunction };
   [key: string]: any;
 }
 
-export function Form(props: FormProps) {
-  const className = [];
-  const { onSubmit } = props;
+export class FormConnect extends React.Component<FormProps> {
+  value: FormValue;
 
-  if (props.showValidation) {
-    className.push("form--show-validation");
+  constructor(props) {
+    super(props);
+    this.value = {};
   }
 
-  if (props.type) {
-    className.push("form--" + props.type);
+  onValue(e: InputValueEvent) {
+    const {
+      onChange,
+    } = this.props;
+
+    console.log(e);
+
+    this.value[e.name] = e.value;
+    if (onChange) {
+      let evt = {
+        type: "formchange",
+        value: this.value
+      };
+
+      if (typeof onChange === "function") {
+        onChange(evt);
+      } else {
+        onChange.handleEvent(evt);
+      }
+    }
   }
 
-  return (
-    <form
-      className={className.join(" ")}
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (onSubmit) {
-          onSubmit(e);
-        }
-      }}
-    >
-      {React.Children.toArray(props.children).map((child: JSX.Element) => {
-        if (typeof child.type === "function") {
-          return React.cloneElement(child, {
-            formid: props.id,
-            onValue: (e) => {
-              if (props.id) {
-                dispatch("FORM_VALUE", {
-                  type: e.type,
-                  value: e.value,
-                  name: e.name,
-                  id: props.id,
-                });
-              }
-            }
-          });
-        }
-        return child;
-      })}
-    </form>
-  );
+  render() {
+    const classList = [];
+
+    const {
+      id,
+      onSubmit,
+      showValidation,
+      type,
+    } = this.props;
+
+    if (showValidation) {
+      classList.push("form--show-validation");
+    }
+
+    if (type) {
+      classList.push("form--" + type);
+    }
+
+    return (
+      <form
+        className={classList.join(" ")}
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (onSubmit) {
+            onSubmit(e);
+          }
+        }}
+      >
+        {React.Children.toArray(this.props.children).map((child: JSX.Element) => {
+          if (typeof child.type === "function") {
+            return React.cloneElement(child, {
+              formid: id,
+              onValue: (e: InputValueEvent) => this.onValue(e),
+            });
+          }
+          return child;
+        })}
+      </form>
+    );
+  }
 }
-
-export const FormConnect = withStore<FormProps>(Form, mapStateToProps)();
