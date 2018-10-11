704be4de92f91c6430717b61d79107cd22129c0d
SeanJM <seanjmacisaac@gmail.com>
2018-10-11 08:01:03 -0700

    Outsourced scrollbar detection to SmartScroll

--- frontend/src/components/viewport/index.tsx
+++ frontend/src/components/viewport/index.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import { SmartScroll } from "@components/smart-scroll";
 
 interface ViewPortProps {
   head?: JSX.Element;
@@ -10,116 +11,62 @@ interface ViewPortProps {
   className?: string;
 }
 
-interface ViewPortState {
-  bodyOverflow: boolean;
-}
+export function Viewport(props: ViewPortProps) {
+  const classList = ["viewport"];
+  const {
+    body,
+    className,
+    feet,
+    head,
+    scopebar,
+    titlebar,
+    toolbar,
+  } = props;
 
-export class Viewport extends Component<ViewPortProps, ViewPortState> {
-  bodyNode: HTMLDivElement;
-  bodyContentNode: HTMLDivElement;
-  bodyOverflowChecker: any;
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      bodyOverflow: false
-    };
+  if (head) {
+    classList.push("viewport--head");
   }
 
-  componentWillUnmount() {
-    clearTimeout(this.bodyOverflowChecker);
+  if (titlebar) {
+    classList.push("viewport--titlebar");
   }
 
-  componentDidMount() {
-    const checker = () => {
-      const { bodyOverflow } = this.state;
-      const bodyContentNodeOffset = this.bodyContentNode.getBoundingClientRect();
-      const bodyNodeOffset = this.bodyNode.getBoundingClientRect();
-      const isBiggerThan = bodyContentNodeOffset.height > bodyNodeOffset.height;
-
-      if (isBiggerThan && !bodyOverflow) {
-        this.setState({
-          bodyOverflow: true
-        });
-      } else if (!isBiggerThan && bodyOverflow) {
-        this.setState({
-          bodyOverflow: false
-        });
-      }
-
-      this.bodyOverflowChecker = setTimeout(checker, 200);
-    };
-
-    checker();
+  if (toolbar) {
+    classList.push("viewport--toolbar");
   }
 
-  render() {
-    const classList = ["viewport"];
-    const {
-      body,
-      className,
-      feet,
-      head,
-      scopebar,
-      titlebar,
-      toolbar,
-    } = this.props;
-
-    if (head) {
-      classList.push("viewport--head");
-    }
-
-    if (titlebar) {
-      classList.push("viewport--titlebar");
-    }
-
-    if (toolbar) {
-      classList.push("viewport--toolbar");
-    }
-
-    if (scopebar) {
-      classList.push("viewport--scopebar");
-    }
-
-    if (feet) {
-      classList.push("viewport--feet");
-    }
-
-    if (className) {
-      classList.push(className);
-    }
-
-    if (this.state.bodyOverflow) {
-      classList.push("viewport--body-overflow");
-    }
-
-    return (
-      <div className={classList.join(" ")}>
-        {head
-          ? <div className="viewport_head">{head}</div>
-          : null}
-        {titlebar
-          ? <div className="viewport_titlebar">{titlebar}</div>
-          : null}
-        {toolbar
-          ? <div className="viewport_toolbar">{toolbar}</div>
-          : null}
-        {scopebar
-          ? <div className="viewport_scopebar">{scopebar}</div>
-          : null}
-        <div
-          ref={(node) => { this.bodyNode = node; }}
-          className="viewport_body">
-          <div
-            ref={(node) => { this.bodyContentNode = node; }}
-            className="viewport_body_content">
-            {body}
-          </div>
-        </div>
-        {feet
-          ? <div className="viewport_feet">{feet}</div>
-          : null}
-      </div>
-    );
+  if (scopebar) {
+    classList.push("viewport--scopebar");
   }
+
+  if (feet) {
+    classList.push("viewport--feet");
+  }
+
+  if (className) {
+    classList.push(className);
+  }
+
+  return (
+    <div className={classList.join(" ")}>
+      {head
+        ? <div className="viewport_head">{head}</div>
+        : null}
+      {titlebar
+        ? <div className="viewport_titlebar">{titlebar}</div>
+        : null}
+      {toolbar
+        ? <div className="viewport_toolbar">{toolbar}</div>
+        : null}
+      {scopebar
+        ? <div className="viewport_scopebar">{scopebar}</div>
+        : null}
+      <SmartScroll className="viewport_body">
+        {body}
+      </SmartScroll>
+      {feet
+        ? <div className="viewport_feet">{feet}</div>
+        : null}
+    </div>
+  );
 }
