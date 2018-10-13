8f085bbd7bcee09b060c032199b774a0e860b1dd
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-13 11:03:37 -0400

    Fixed history keeping old value when updated too quickly

--- frontend/src/components/router/history.tsx
+++ frontend/src/components/router/history.tsx
@@ -4,30 +4,34 @@ import { RouterLocation } from "@frontend/components/router/types";
 import getLocation from "@frontend/components/router/get-location";
 import path, { PathQueryValue } from "@path";
 
-export interface HistoryLocation extends Partial<RouterLocation> {
-  query?: PathQueryValue;
+export interface HistoryLocation extends RouterLocation {
+  query: PathQueryValue;
 }
 
 export type RouterHistory = typeof history;
 
-function pushObject(location: HistoryLocation) {
+function pushObject(location: Partial<HistoryLocation>) {
   let prevLocation = history.last();
   const newLocation: HistoryLocation = {...prevLocation, ...location};
-  let newUrl = newLocation.pathname;
+  let href = newLocation.pathname;
 
   if (newLocation.query) {
-    newUrl += path.query().assign(newLocation.query).toString();
+    href += path.query().assign(newLocation.query).toString();
   } else if (newLocation.search) {
-    newUrl += (newLocation.search[0] === "?" ? "" : "?") + newLocation.search;
+    href += (newLocation.search[0] === "?" ? "" : "?") + newLocation.search;
   }
 
   if (newLocation.hash) {
-    newUrl += "#" + newLocation.hash;
+    href += "#" + newLocation.hash;
   }
-  window.location.hash = "#" + newUrl;
+
+  newLocation.href = href;
+  history.stack.push(newLocation);
+  history.__updateHashChange = false;
+  window.location.hash = "#" + href;
 }
 
-function push(url: HistoryLocation): void;
+function push(url: Partial<HistoryLocation>): void;
 function push(url: string): void;
 function push(url: any) {
   if (typeof url === "string") {
@@ -38,20 +42,24 @@ function push(url: any) {
   }
 }
 
-function handleEvent() {
-  history.stack.push(
-    getLocation(window.location.hash.substring(1))
-  );
-}
-
 export const history = {
+  __updateHashChange: true,
+
   stack: [
     getLocation(window.location.hash.substring(1))
   ],
 
   last: () => history.stack.slice(-1)[0],
-  handleEvent,
   push,
 };
 
-window.addEventListener("hashchange", history);
+window.addEventListener("hashchange", () => {
+  if (history.__updateHashChange) {
+    let href = window.location.hash.substring(1);
+    history.stack.push(
+      getLocation(href)
+    );
+  } else {
+    history.__updateHashChange = true;
+  }
+});
