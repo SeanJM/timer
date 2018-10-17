256c1491cf11fe6b910d2e8db4af693bfda99b5b
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-17 07:32:20 -0400

    Added alert animation

--- frontend/src/components/alert/alert.tsx
+++ frontend/src/components/alert/alert.tsx
@@ -1,16 +1,50 @@
 import React from "react";
+import anime from "animejs";
 import { Titlebar } from "@components/titlebar";
+import { KEYNAME_BY_CODE } from "@constants";
 
 interface AlertProps {
   title: string;
   content: JSX.Element;
   control: JSX.Element;
+  onEsc?: () => void;
 }
 
 export class Alert extends React.Component<AlertProps> {
+  node: HTMLDivElement;
+
+  handleEvent(e: KeyboardEvent) {
+    if (KEYNAME_BY_CODE[e.which] === "ESC" && this.props.onEsc) {
+      this.props.onEsc();
+    }
+  }
+
+  componentDidMount() {
+    document.body.addEventListener("keydown", this);
+
+    anime({
+      targets: [this.node],
+
+      scale: {
+        value: [ 0.8, 1 ],
+        duration: 600,
+        elasticity: 300,
+      },
+
+      opacity: {
+        value: [ 0, 1 ],
+        easing: "easeOutQuad",
+        duration: 300,
+      },
+    });
+  }
+
   render() {
     return (
-      <div className="alert">
+      <div
+        className="alert"
+        ref={(node) => { this.node = node; }}
+      >
         <Titlebar>
           <h6>{this.props.title}</h6>
         </Titlebar>
