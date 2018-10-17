c1802daba9edb9b58b176011ed9a8d5548c9c3df
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-17 07:33:33 -0400

    Todo dispatch types

--- frontend/src/action/todo/index.ts
+++ frontend/src/action/todo/index.ts
@@ -1,6 +1,38 @@
 import { TodoService } from "@frontend/action/todo/service";
 
-export function todo(subscribe) {
+export interface Subscribe {
+  (name: "TODO", callback: (e: { type: string, value: any }) => void): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "ADD", value: { name: string, tags: string[], categoryID: string } }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "EDIT", value: any }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "DELETE", value: { categoryID: string, idList: string[] } }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "DESELECT", value: { categoryID: string, idList: string[] } }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "SELECT", value: { categoryID: string, idList: string[] } }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "INCOMPLETE", value: { categoryID: string, idList: string[] } }): void;
+}
+
+export interface TodoDispatch {
+  (name: "TODO", e: { type: "COMPLETE", value: { categoryID: string, idList: string[] } }): void;
+}
+
+export function todo(subscribe: Subscribe) {
   const service = new TodoService();
 
   subscribe("TODO", ({ type, value }) => {
