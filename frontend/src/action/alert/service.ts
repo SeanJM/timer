103fce506519061bb1e5b85fa71d0ccd953a0cac
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-18 08:02:09 -0400

    alert will generate an ID if it does not exist

--- frontend/src/action/alert/service.ts
+++ frontend/src/action/alert/service.ts
@@ -1,7 +1,12 @@
 import { store, StoreAlert } from "@store";
+import generateHash from "@generate-hash";
 
 export class AlertService {
   push(alert: StoreAlert) {
+    if (!alert.id) {
+      alert.id = generateHash();
+    }
+
     store.set({
       alerts: store.value.alerts.concat(alert)
     });
