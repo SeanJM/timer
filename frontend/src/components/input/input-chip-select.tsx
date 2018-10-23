4f2e709d1b7f1ff888bfc0f2448802a8c1e13fe3
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-10-23 08:23:30 -0400

    Cleaned

--- frontend/src/components/input/input-chip-select.tsx
+++ frontend/src/components/input/input-chip-select.tsx
@@ -1,32 +1,23 @@
 import React from "react";
-import { ChipInput, ChipData } from "@frontend/components/chip";
+import { ChipInput, ChipData, ChipSubmitEvent } from "@frontend/components/chip";
 import { inputWrapper } from "@frontend/components/input/input-wrapper";
 import { InputValueEvent } from "@types";
 
 export interface ChipSelectProps {
   data: ChipData[];
+  defaultValue?: string[];
   formid?: string;
   name?: string;
-  defaultValue?: string[];
-
   onBlur: (e?: React.FocusEvent) => void;
   onFocus: (e?: React.FocusEvent) => void;
   onInput?: (e?: InputValueEvent) => void;
+  onSubmit?: (e: ChipSubmitEvent) => void;
   onValue: (e: InputValueEvent) => void;
 }
 
 function InputChipSelectView(props: ChipSelectProps) {
   return (
-    <ChipInput
-      data={props.data}
-      defaultValue={props.defaultValue}
-      name={props.name}
-
-      onBlur={props.onBlur}
-      onFocus={props.onFocus}
-      onInput={props.onInput}
-      onValue={props.onValue}
-    />
+    <ChipInput {...props}/>
   );
 }
 
